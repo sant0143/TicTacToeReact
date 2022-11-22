19cae7af1693e60dd4aef2ccc0143873024172f7
sant0143 <santosh25.yeshwanth@gmail.com>
2022-11-22 17:42:34 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+// import TicTacToe from './components/TicTacToe/TicTacToe'
 
-function App() {
+
+function Counter() {
+  const[plus,setPlus] = useState(0)
+  const[count,setCount] = useState(0)
+  
+  //similar to componentDidMount
+  // useEffect(function(){
+  //   document.title=`You clicked ${plus} times`
+  // },[])
+
+  //similar to componentDidUpdate
+  useEffect(function(){
+      document.title=`You clicked ${count} times`
+    },[plus,count]) //empty dependency array
+
+  const Increment = ()=>{
+    setPlus(plus + 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginLeft : '20px'}}>
+      <h1>Counter : {plus}</h1>
+      <button onClick={Increment}>Increment</button>
+      <button onClick={()=>setCount(count+1)}>Increment 2</button>
     </div>
-  );
+  )
 }
 
-export default App;
+
+function App() {
+  const[showCounter,setShowCounter] = useState(false)
+  return (
+    <div className='app'>
+      {/* <TicTacToe/> */}
+      {
+        showCounter && <Counter/>
+      }
+      <button onClick={()=>{setShowCounter(true)}}>Mount</button>
+      <button onClick={()=>{setShowCounter(false)}}>Unmount</button>
+    </div>
+  )
+}
+
+export default App
